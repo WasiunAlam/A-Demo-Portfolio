edb1e6dec07e37d2c77449430fe402de69547674
Wasiun <alphawasiun@gmail.com>
2025-03-23 19:50:57 +0600

    Fixing some js

--- script.js
+++ script.js
@@ -6,27 +6,6 @@ toggleBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-function handleTilt() {
-    const isPortrait = window.matchMedia("(orientation: portrait)").matches;
-    const tiltElements = document.querySelectorAll(".tilt");
-  
-    if (isPortrait) {
-      tiltElements.forEach((el) => {
-        el.vanillaTilt?.destroy(); // Destroy Tilt.js if it exists
-      });
-    } else {
-      VanillaTilt.init(tiltElements, {
-        max: 25,
-        speed: 400,
-        glare: true,
-        "max-glare": 0.5,
-      });
-    }
-  }
-  
-  // Run on load and resize
-  window.addEventListener("load", handleTilt);
-  window.addEventListener("resize", handleTilt);
 
 
   ScrollReveal().reveal('.identity-section',{ 
